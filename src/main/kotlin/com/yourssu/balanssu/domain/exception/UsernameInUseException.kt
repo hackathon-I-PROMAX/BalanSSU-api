b3421409c9773302ebc56bcb999186d38349aa96
mike-urssu <mike.urssu@gmail.com>
2023-02-19 11:52:11 +0900

    fix: 아이디 유효성 검사 실패하면 400 상태 반환

--- src/main/kotlin/com/yourssu/balanssu/domain/exception/UsernameInUseException.kt
+++ src/main/kotlin/com/yourssu/balanssu/domain/exception/UsernameInUseException.kt
@@ -4,7 +4,7 @@ import com.yourssu.balanssu.domain.errorCode.UserErrorCode
 import org.springframework.http.HttpStatus
 
 class UsernameInUseException : UserException(
-    HttpStatus.CONFLICT,
+    HttpStatus.BAD_REQUEST,
     UserErrorCode.USERNAME_IN_USE,
     "이미 사용중인 아이디입니다."
 )
