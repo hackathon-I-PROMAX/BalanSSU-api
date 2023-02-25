d9fa7f2278db9554ca7771f9384aadf91b55c7b0
mike-urssu <mike.urssu@gmail.com>
2023-02-25 16:20:16 +0900

    hot-fix: 회원 정보 유효성 검사 수정

--- src/main/kotlin/com/yourssu/balanssu/application/request/SignUpRequest.kt
+++ src/main/kotlin/com/yourssu/balanssu/application/request/SignUpRequest.kt
@@ -14,7 +14,7 @@ class SignUpRequest(
     val schoolAge: String,
     val departure: String,
     @field:Size(min = 1, max = 1, message = "성별은 반드시 'M' 또는 'F' 중 하나를 입력해야 합니다.")
-    val gender: Char
+    val gender: String
 ) {
     companion object {
         const val PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)[!-~₩]{8,100}$"
