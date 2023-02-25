f0d95703fc6bd4c3ed9701755614bf4533295772
mike-urssu <mike.urssu@gmail.com>
2023-02-25 15:59:32 +0900

    hot-fix: 회원 정보 유효성 검사 수정

--- src/main/kotlin/com/yourssu/balanssu/application/request/ValidateNicknameRequest.kt
+++ src/main/kotlin/com/yourssu/balanssu/application/request/ValidateNicknameRequest.kt
@@ -3,6 +3,6 @@ package com.yourssu.balanssu.application.request
 import javax.validation.constraints.Size
 
 class ValidateNicknameRequest(
-    @field:Size(min = 5, max = 64, message = "5자리 이상, 64자리 이하의 닉네임을 입력해야 합니다.")
+    @field:Size(min = 3, max = 64, message = "3자리 이상, 64자리 이하의 닉네임을 입력해야 합니다.")
     val nickname: String
 )
