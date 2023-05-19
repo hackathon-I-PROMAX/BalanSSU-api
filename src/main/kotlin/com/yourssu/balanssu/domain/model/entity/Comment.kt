019659e6202b450bf0adfce81d44ba4f75aef182
mike-urssu <mike.urssu@gmail.com>
2023-05-19 13:38:37 +0900

    entity: Comment에 @Where 'is_deleted = 0' 추가

--- src/main/kotlin/com/yourssu/balanssu/domain/model/entity/Comment.kt
+++ src/main/kotlin/com/yourssu/balanssu/domain/model/entity/Comment.kt
@@ -8,9 +8,11 @@ import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 import javax.persistence.Table
+import org.hibernate.annotations.Where
 
 @Entity
 @Table
+@Where(clause = "is_deleted = 0")
 class Comment(
     @ManyToOne
     @JoinColumn(name = "category_id")
